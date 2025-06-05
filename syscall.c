2b952efad6f552831114d6c2e929926c1810ca11
minhyeon <guncraft11@gmail.com>
2025-06-05 11:37:01 +0900

    register syscall function pointer of setnice, getnice, ps in syscall.c > syscalls arr

--- syscall.c
+++ syscall.c
@@ -131,6 +131,9 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_yield]   sys_yield,
+[SYS_setnice] sys_setnice,
+[SYS_getnice] sys_getnice,
+[SYS_ps]      sys_ps,
 };
 
 void
